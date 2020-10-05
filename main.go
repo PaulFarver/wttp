05b743c552cced943dc3fde836e6f0bc4f80e62f
Paul Farver <paulfarver@gmail.com>
2020-10-05 16:57:14 +0200

    Add a little bit of error handling
    Signed-off-by: Paul Farver <paulfarver@gmail.com>

--- main.go
+++ main.go
@@ -8,10 +8,15 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Could not parse command line arguments: ", os.Args[1:])
+		os.Exit(1)
+	}
 	arg := os.Args[1]
 	code, err := strconv.Atoi(arg)
 	if err != nil {
 		fmt.Println("Code is not an integer: ", err)
+		os.Exit(1)
 	}
 	fmt.Println(http.StatusText(code))
 }
